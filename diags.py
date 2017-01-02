fb94fbe4d7caaaf748ab9263a178806322fdad17
Joe Buehl <joe@thebuehls.com>
2017-01-02 15:40:41 -0800

    Misc changes

--- diags.py
+++ diags.py
@@ -1,4 +1,3 @@
-# https://en.wikipedia.org/wiki/OBD-II_PIDs
 
 import serial
 import json
@@ -16,7 +15,10 @@ stateFileName = stateDir+"diags.json"
 diagState = {"Battery": 0.0,
              "Rpm": 0, 
              "WaterTemp": 0,
-             "AirTemp": 0}
+             "Barometer": 0.0,
+             "AirTemp": 0,
+             "RunTime": 0,
+             }
 
 def sendMsg(port, msg):
     if debug: print msg
@@ -51,7 +53,7 @@ def readData(port, mode, pid):
     elif len(response) == 12:
         fmt = "!BBL"
     else:
-        return resp[4:]
+        return response[4:]
     (respMode, respPid, respData) = struct.unpack(fmt, response.decode("hex"))
     return respData
 
@@ -69,6 +71,9 @@ def writeLog(logFile, diagState):
     logFile.flush()
 
 # initialize
+#
+# https://cdn.sparkfun.com/assets/c/8/e/3/4/521fade6757b7fd2768b4574.pdf
+
 logFile = logFile = open(logDir+time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))+"-"+logFileName, "w")
 writeState(stateFileName, diagState)
 diagPort = serial.Serial(diagDevice, baudrate=diagBaud)
@@ -78,18 +83,30 @@ sendMsg(diagPort, "ate0")       # echo off
 msg = readMsg(diagPort)
 sendMsg(diagPort, "atsp0")      # set protocol 0
 msg = readMsg(diagPort)
+sendMsg(diagPort, "03")         # get diag codes
+msg = readMsg(diagPort)
+sendMsg(diagPort, "0902")       # get VIN
+msg = readMsg(diagPort)
 
 # get supported PIDs
-
+#
+# PID support query responses:
 # 4100BF9FA893
 # 41209105B11F
 # 4140FADC2000
-
+#
+# Supported PID bits:
 # BF9FA8939105B11FFADC2000
-
-# 00, 02, 04, 05, 06, 07, 08, 09, 0c, 0d, 0e, 0f, 10, 11, 13, 15, 19, 1c, 1f, 20
-# 21, 28, 2e, 30, 31, 33, 34, 38, 3c, 3d, 3e, 3f, 40
-# 41, 42, 43, 44, 45, 47, 49, 4a, 4c, 4d, 4e, 53
+#
+# Supported PIDs:
+# 00,     02,     04, 05, 06, 07, 08, 09,         0c, 0d, 0e, 0f, 
+# 10, 11,     13,     15,             19,         1c,         1f,
+# 20, 21,                         28,                     2e,     
+# 30, 31,     33, 34,             38,             3c, 3d, 3e, 3f,
+# 40, 41, 42, 43, 44, 45,     47,     49, 4a,     4c, 4d, 4e,    
+#             53
+#
+# https://en.wikipedia.org/wiki/OBD-II_PIDs
 
 pids = [readData(diagPort, 1, 0x00),
         readData(diagPort, 1, 0x20),
@@ -97,19 +114,19 @@ pids = [readData(diagPort, 1, 0x00),
 #        readData(diagPort, 1, 0x60),
 #        readData(diagPort, 1, 0x80),
 #        readData(diagPort, 1, 0xa0),
-#        readData(diagPort, 1, 0xc0)
+#        readData(diagPort, 1, 0xc0),
         ]
-#if debug: print "%08x "*7 % tuple(pids)
+if debug: print "%08x "*len(pids) % tuple(pids)
 
 # read vehicle data
 while True:
+    diagState["Battery"] = readBattery(diagPort)
     diagState["WaterTemp"] = readData(diagPort, 1, 0x05) - 40
     diagState["Rpm"] = readData(diagPort, 1, 0x0c) / 4
     diagState["Speed"] = readData(diagPort, 1, 0x0d) * 0.621371
     diagState["IntakeTemp"] = readData(diagPort, 1, 0x0f) - 40
     diagState["RunTime"] = readData(diagPort, 1, 0x1f)
     diagState["Barometer"] = readData(diagPort, 1, 0x33) * 0.2953
-    diagState["Battery"] = readBattery(diagPort)
     
     writeState(stateFileName, diagState)
     writeLog(logFile, diagState)
--- gps.py
+++ gps.py
@@ -2,30 +2,39 @@ import sys
 import os
 import time
 import json
+import timezonefinder
+import pytz
+import datetime
 
+# configuration
+debug = False
 rootDir = "/root/data/"
 logDir = rootDir+"gps/"
 stateDir = rootDir
-logFileName = "gps.csv"
+logFileBaseName = "gps.csv"
 stateFileName = "gps.json"
 gpsDevice = "/dev/ttyUSB0"
-minSpeed = 2        # ignore speed less than this value
+minSats = 0             # number of satellites required to start writing data
+minSpeed = 5.0          # ignore speed if less than this value
 
+# global variables
 nSats = 0
-dateStamp = "000000"
-timeStamp = "000000"
+#dateStamp = "000000"
+#timeStamp = "000000"
 latitude = 0.0
-latDir = ""
+#latDir = ""
 longitude = 0.0
-longDir = ""
+#longDir = ""
 position = (0, "", 0, "")
 altitude = 0.0
 speed = 0.0
 heading = 0.0
 now = time.struct_time((2000, 1, 1, 0, 0, 0, 0, 0, 0))
 lastTime = now
+timeZone = "America/Los_Angeles"
 logFile = None
-inFile = open(gpsDevice)
+inFile = None
+first = True
 
 # convert date/time stamps to current time
 def curTime(dateStamp, timeStamp, now):
@@ -40,15 +49,24 @@ def curTime(dateStamp, timeStamp, now):
     hour = int(timeStamp[0:2])
     minute = int(timeStamp[2:4])
     second = int(timeStamp[4:6])
-    return time.struct_time((year, month, day, hour, minute, second,0 ,0 ,0))
+    return time.struct_time((year, month, day, hour, minute, second, 0, 0, 0))
 
 # set the system time
 def setTime(now):
     print "Setting system time"
     os.system('date -s "'+time.asctime(now)+'"')
 
+# parsing routines
 def parsePosition(latStr, latDir, longStr, longDir):
-    return (str2float(latStr)/100, latDir, str2float(longStr)/100, longDir)
+    return (nmea2deg(str2float(latStr)), latDir, nmea2deg(str2float(longStr)), longDir)
+
+def parseLatitude(latStr, latDir):
+    latitude = nmea2deg(str2float(latStr))
+    return (latitude if latDir == "N" else -latitude)
+
+def parseLongitude(longStr, longDir):
+    longitude = nmea2deg(str2float(longStr))
+    return (longitude if latDir == "E" else -longitude)
 
 def parseAltitude(altStr):
     return str2float(altStr)*3.28084
@@ -58,6 +76,11 @@ def parseSpeed(speedStr):
 
 def parseHeading(trackStr):
     return str2float(trackStr)
+
+# convert a NMEA location value ddmm.mmmm to degrees dd.dddddd
+def nmea2deg(value):
+    deg = int(value/100)
+    return deg + (value - deg*100)/60
       
 def str2float(value):
     try: return float(value)
@@ -67,66 +90,100 @@ def str2int(value):
     try: return int(value)
     except: return 0
 
-def formatTime(now):
-    return time.strftime('%Y-%m-%d %H:%M:%S', now)
+# formatting routines
+def formatTime(now, tz):
+    fmt = "%Y-%m-%d %H:%M:%S"
+#    return datetime.datetime(*now[0:6], tzinfo=pytz.utc).astimezone(pytz.timezone(tz)).strftime(fmt)
+    return datetime.datetime(*now[0:6], tzinfo=pytz.utc).strftime(fmt)
 
 def formatPosition(position):
     return "%10.6f %s %10.6f %s" % (position[0], position[1], position[2], position[3])
         
 def formatPrint(now, position, altitude, speed, heading, nSats):
      return "%s Position: %s  Altitude: %5d Speed: %3d Heading: %03d %d satellites" % \
-            (formatTime(now), formatPosition(position), altitude, speed, heading, nSats)
+            (formatTime(now, timeZone), formatPosition(position), altitude, speed, heading, nSats)
 
-def formatJson(now, position, altitude, speed, heading, nSats):
-    return json.dumps({"Time": formatTime(now), "Pos": formatPosition(position), "Alt": altitude, "Speed": speed, "Hdg": heading, "Nsats": nSats})
+#def formatJson(now, timeZone, position, altitude, speed, heading, nSats):
+def formatState():
+    return json.dumps({"Time": formatTime(now, timeZone), "TimeZone": timeZone, "Pos": formatPosition(position), "Lat": latitude, "Long": longitude, "Alt": altitude, "Speed": speed, "Hdg": heading, "Nsats": nSats})
 
-while True:
-    inMsg = inFile.readline()
-    if logFile:
-        logFile.write(inMsg)
-        logFile.flush()
-    gpsMsg = inMsg.rstrip("\n").split(",")
-    if gpsMsg[0] == "$GPGGA":
+if __name__ == "__main__":
+    # open the serial port the gps device is connected to
+    while not inFile:
         try:
-            (timeStamp, latStr, latDir, longStr, longDir, quality, nSatsStr, dilution, altStr, altUnits, geoid, geoidUnits, whatever, chksum) = gpsMsg[1:15]
-            now = curTime("", timeStamp, now)
-            position = parsePosition(latStr, latDir, longStr, longDir)
-            altitude = parseAltitude(altStr)
-            nSats = str2int(nSatsStr)
+            inFile = open(gpsDevice)
+            if debug: print "opened gps device", gpsDevice
         except:
+            if debug: print "waiting for gps device", gpsDevice
+            time.sleep(1)
+
+    timeZoneFinder = timezonefinder.TimezoneFinder()
+
+    # read the gps data
+    while True:
+        inMsg = inFile.readline()
+
+        # write to the log file if it is open which occurs after the time has been acquired
+        if logFile:
+            logFile.write(inMsg)
+            logFile.flush()
+            
+        # parse the message
+        gpsMsg = inMsg.rstrip("\n").split(",")
+        if gpsMsg[0] == "$GPGGA":
+            try:
+                (timeStamp, latStr, latDir, longStr, longDir, quality, nSatsStr, dilution, altStr, altUnits, geoid, geoidUnits, whatever, chksum) = gpsMsg[1:15]
+                now = curTime("", timeStamp, now)
+                position = parsePosition(latStr, latDir, longStr, longDir)
+                latitude = parseLatitude(latStr, latDir)
+                longitude = parseLongitude(longStr, longDir)
+                altitude = parseAltitude(altStr)
+                nSats = str2int(nSatsStr)
+            except:
+                if debug: raise
+        elif gpsMsg[0] == "$GPGSA":
             pass
-    elif gpsMsg[0] == "$GPGSA":
-        pass
-    elif gpsMsg[0] == "$GPGSV":
-        pass
-    elif gpsMsg[0] == "$GPGLL":
-        pass
-    elif gpsMsg[0] == "$GPRMC":
-        try:
-            (timeStamp, status, latStr, latDir, longStr, longDir, speedStr, trackStr, dateStamp, magVar, chksum) = gpsMsg[1:12]
-            now = curTime(dateStamp, timeStamp, now)
-            position = parsePosition(latStr, latDir, longStr, longDir)
-            speed = parseSpeed(speedStr)
-            if speed > 0:   # retain last heading if stopped
-                heading = parseHeading(trackStr)
-        except:
+        elif gpsMsg[0] == "$GPGSV":
             pass
-    elif gpsMsg[0] == "$GPVTG":
-        pass
-#    else:
-#        if gpsMsg[0] != "":
-#            print gpsMsg[0]
+        elif gpsMsg[0] == "$GPGLL":
+            pass
+        elif gpsMsg[0] == "$GPRMC":
+            try:
+                (timeStamp, status, latStr, latDir, longStr, longDir, speedStr, trackStr, dateStamp, magVar, chksum) = gpsMsg[1:12]
+                now = curTime(dateStamp, timeStamp, now)
+                position = parsePosition(latStr, latDir, longStr, longDir)
+                latitude = parseLatitude(latStr, latDir)
+                longitude = parseLongitude(longStr, longDir)
+                speed = parseSpeed(speedStr)
+                if speed > 0.0:   # retain last heading if stopped
+                    heading = parseHeading(trackStr)
+            except:
+                if debug: raise
+        elif gpsMsg[0] == "$GPVTG":
+            pass
+        else:
+            if gpsMsg[0] != "":
+                if debug: print "unknown gps message", gpsMsg[0]
+            
+        if now != lastTime: # do this once per second
+            lastTime = now
+            if now[0] > 2000:   # don't write anything until valid time has been acquired
+                if first or (now[5] == 0): # set system time initially, then once per minute
+                    timeZone = timeZoneFinder.timezone_at(lat=position[0] if position[1] == "N" else -position[0], lng=position[2] if position[3] == "E" else -position[2])
+                    if debug: print "time zone", timeZone
+                    # update the system time (always UTC)
+                    if debug: print "setting time", now
+                    setTime(now)
+                    if debug: print formatPrint(now, position, altitude, speed, heading, nSats)
+                    first = False
+                if not logFile: # open a new log file if it hasn't happened yet
+                    logFileName = logDir+time.strftime('%Y%m%d%H%M%S', now)+"-"+logFileBaseName
+                    if debug: print "opening log file", logFileName
+                    logFile = open(logFileName, "w")
+                if logFile:
+                    if nSats >= minSats:   # update the state file if we have acquired enough satellites
+                        with open(stateDir+stateFileName, "w") as stateFile:
+                            stateFile.write(formatState()) # formatJson(now, timeZone, position, altitude, speed, heading, nSats))
+            else:
+                if debug: print "waiting for date", now[0]
         
-    if now != lastTime: # do this once per second
-        lastTime = now
-        if now[0] > 2000:   # don't write anything until valid time has been acquired
-            if time.gmtime(time.time()).tm_year <= 2000:
-                # set the system time
-                setTime(now)
-            if not logFile:
-                logFile = open(logDir+time.strftime('%Y%m%d%H%M%S', now)+"-"+logFileName, "w")
-            if logFile:
-                if nSats > 0:
-                    with open(stateDir+stateFileName, "w") as stateFile:
-                        stateFile.write(formatJson(now, position, altitude, speed, heading, nSats))
-    
